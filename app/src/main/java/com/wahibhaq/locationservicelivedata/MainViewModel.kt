b0fcd30f6f6d1c519a61b0c1ed0c3ee03334266b
wahibhaq <wahib.tech@gmail.com>
2018-08-06 14:44:36 +0200

    kotlin refactor

--- app/src/main/java/com/wahibhaq/locationservicelivedata/MainViewModel.kt
+++ app/src/main/java/com/wahibhaq/locationservicelivedata/MainViewModel.kt
@@ -2,10 +2,9 @@ package com.wahibhaq.locationservicelivedata
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.LiveData
 
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    val gpsStatusLiveData: LiveData<GpsStatus> = GpsStatusListener(application)
+    val gpsStatusLiveData = GpsStatusListener(application)
 }
